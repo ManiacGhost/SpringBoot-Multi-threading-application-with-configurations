c6c2a5397bad49d1d0ba713ebaacb5cac8c4dde0
Harsh Pandey <76989208+ManiacGhost@users.noreply.github.com>
2025-05-19 14:32:07 +0530

    initialized the project

---
+++ threadScheduler/src/main/java/com/example/threadScheduler/ThreadSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.example.threadScheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThreadSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThreadSchedulerApplication.class, args);
+	}
+
+}
---
+++ threadScheduler/src/test/java/com/example/threadScheduler/ThreadSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.threadScheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThreadSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
