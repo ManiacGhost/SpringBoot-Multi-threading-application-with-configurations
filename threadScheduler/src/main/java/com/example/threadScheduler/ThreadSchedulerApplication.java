c08b11983491ab0194aaa6471a31ee42bfb46fe5
Martand Mahajan <martandmahajan03@gmail.com>
2025-06-24 12:13:50 +0530

    added cors

--- threadScheduler/src/main/java/com/example/threadScheduler/ThreadSchedulerApplication.java
+++ threadScheduler/src/main/java/com/example/threadScheduler/ThreadSchedulerApplication.java
@@ -2,6 +2,9 @@ package com.example.threadScheduler;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ThreadSchedulerApplication {
@@ -10,4 +13,18 @@ public class ThreadSchedulerApplication {
 		SpringApplication.run(ThreadSchedulerApplication.class, args);
 	}
 
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer(){
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS","PATCH")
+						.allowedOrigins("http://localhost:5173")
+						.allowedHeaders("*")
+						.allowCredentials(true);
+			}
+		};
+	}
 }
